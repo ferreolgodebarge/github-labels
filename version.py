413b9b7e86b71aa1924ad876a1bd018e0cf603e4
Ferreol <ferreolgodebarge@gmail.com>
2019-02-26 00:59:31 +0100

    ok

--- version.py
+++ version.py
@@ -5,13 +5,14 @@ import sys
 import json
 import requests
 
+
 def get_label(input_json):
     data = json.loads(input_json)
-    labels = data['labels']
+    labels = data["labels"]
     labels_list = []
     for label in labels:
-        name = label['name']
-        if name == 'patch' or name =='minor' or name == 'major':
+        name = label["name"]
+        if name == "patch" or name == "minor" or name == "major":
             labels_list.append(name)
 
     if len(labels_list) == 1:
@@ -20,23 +21,25 @@ def get_label(input_json):
         print("Error: Zero or more than two versioning labels")
         sys.exit(1)
 
+
 def increment_version(url, input_file):
     input_json = requests.get(url).text
     label = get_label(input_json)
-    with open(input_file, 'r') as input:
+    with open(input_file, "r") as input:
         version = input.readline()
-        [major, minor, patch] = list(map(int, version.split('.')))
-        if label == 'patch':
+        [major, minor, patch] = list(map(int, version.split(".")))
+        if label == "patch":
             patch += 1
-        elif label == 'minor':
+        elif label == "minor":
             minor += 1
-        elif label == 'major':
+        elif label == "major":
             major += 1
-        final_version = '.'.join(list(map(str, [major, minor, patch])))
+        final_version = ".".join(list(map(str, [major, minor, patch])))
 
-    with open(input_file, 'w') as input:
+    with open(input_file, "w") as input:
         input.write(final_version)
     return final_version
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print(increment_version(sys.argv[1], sys.argv[2]))
